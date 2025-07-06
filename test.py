5fe337af818f737de11e3003177a731c9228d951
Dylan Wu <dylanwu2588@gmail.com>
2025-07-05 22:29:22 -0400

    test

---
+++ test.py
@@ -0,0 +1,34 @@
+import asyncio
+from poke_env import RandomPlayer
+from poke_env import LocalhostServerConfiguration
+
+# config
+BATTLE_FORMAT = "gen9randombattle"
+N_BATTLES = 5
+
+# connect to server
+server_config = LocalhostServerConfiguration
+
+async def main():
+    # create 2 random players
+    player1 = RandomPlayer(
+        battle_format=BATTLE_FORMAT,
+        server_configuration=server_config,
+        max_concurrent_battles=1
+    )
+    player2 = RandomPlayer(
+        battle_format=BATTLE_FORMAT,
+        server_configuration=server_config,
+        max_concurrent_battles=1
+    )
+    player1._username = "Bot_A"
+    player2._username = "Bot_B"
+    await player1.battle_against(player2, n_battles=N_BATTLES)
+
+    print(f"\nResults after {N_BATTLES} battles:")
+    print(f"{player1._username} won {player1.n_won_battles}")
+    print(f"{player2._username} won {player2.n_won_battles}")
+    #print results
+
+if __name__ == "__main__":
+    asyncio.run(main())
