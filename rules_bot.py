54ab50661ffa6ac66aa36940b74fd997a6e96dde
Dylan Wu <dylanwu2588@gmail.com>
2025-08-08 22:52:33 -0400

    created damage estimation function

--- rules_bot.py
+++ rules_bot.py
@@ -22,4 +22,19 @@ class RulesBot(Player):
             return self.create_order(max(battle.available_moves, key=lambda move: move.base_power * get_effectiveness(move.type, battle.opponent_active_pokemon.types), default=None))
         #if no moves available, choose a random move
         return self.choose_random_move(battle) 
-    
+    def damage_estimate(self, move: Move, battle: Battle):
+        #function to estimate damage of a move
+        effectiveness = get_effectiveness(move.type, battle.opponent_active_pokemon.types)
+        #check if attacking stat is physical or special
+        attack_stat = battle.active_pokemon.attack if move.category == "Physical" else battle.active_pokemon.special_attack
+        #uses stat formula to calculate effective attacking stat and defending stat of opposing pokemon
+        attack_stat = ((2 * attack_stat + 52) * battle.active_pokemon.level / 100 + 5)
+        return move.base_power * effectiveness * Pokemon.damage_multiplier(battle.active_pokemon, move)
+    def max_damage_move(self, battle: Battle):
+        #function to find the move with maximum damage
+        return max(
+            battle.available_moves,
+            key=lambda move: move.base_power * get_effectiveness(move.type, battle.opponent_active_pokemon.types) * Pokemon.damage_multiplier(battle.active_pokemon, move),
+            default=None
+        )
+    def can_ko(sel)
