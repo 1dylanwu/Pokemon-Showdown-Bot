99edfac87a4ac18fee23d23df919b0f6eef2d883
Dylan Wu <dylanwu2588@gmail.com>
2025-08-09 23:55:48 -0400

    created functions to calculate speed and check if we can ko opponent

--- rules_bot.py
+++ rules_bot.py
@@ -1,9 +1,11 @@
 from poke_env import Player
 from poke_env.battle import Move, Pokemon, Battle
+from poke_env.calc import calculate_damage, calculate_base_power
 from type_chart import get_effectiveness
 
 class RulesBot(Player):
     def choose_move(self, battle: Battle):
+
         #if we have bad defensive type matchup, switch out
         defensive_switch = False
         for enemy_type in battle.opponent_active_pokemon.types:
@@ -22,19 +24,47 @@ class RulesBot(Player):
             return self.create_order(max(battle.available_moves, key=lambda move: move.base_power * get_effectiveness(move.type, battle.opponent_active_pokemon.types), default=None))
         #if no moves available, choose a random move
         return self.choose_random_move(battle) 
-    def damage_estimate(self, move: Move, battle: Battle):
-        #function to estimate damage of a move
-        effectiveness = get_effectiveness(move.type, battle.opponent_active_pokemon.types)
-        #check if attacking stat is physical or special
-        attack_stat = battle.active_pokemon.attack if move.category == "Physical" else battle.active_pokemon.special_attack
-        #uses stat formula to calculate effective attacking stat and defending stat of opposing pokemon
-        attack_stat = ((2 * attack_stat + 52) * battle.active_pokemon.level / 100 + 5)
-        return move.base_power * effectiveness * Pokemon.damage_multiplier(battle.active_pokemon, move)
+    
     def max_damage_move(self, battle: Battle):
         #function to find the move with maximum damage
         return max(
             battle.available_moves,
-            key=lambda move: move.base_power * get_effectiveness(move.type, battle.opponent_active_pokemon.types) * Pokemon.damage_multiplier(battle.active_pokemon, move),
+            key=lambda move: calculate_damage(
+                move=move,
+                attacker=battle.active_pokemon,
+                defender=battle.opponent_active_pokemon,
+                battle=battle
+            )[0],
             default=None
         )
-    def can_ko(sel)
+    
+    def can_ko(self, battle: Battle):
+        #function to check if we can KO the opponent with any move
+        for move in battle.available_moves:
+            possible_damage = calculate_damage(
+                move=move,
+                attacker=battle.active_pokemon,
+                defender=battle.opponent_active_pokemon,
+                battle=battle
+            )
+            # Check if the average damage is enough to KO the opponent
+            if (possible_damage[0] + possible_damage[1]) / 2 >= battle.opponent_active_pokemon.current_hp:
+                return True
+        return False
+    
+    def speed(self, pokemon: Pokemon, battle: Battle):
+        #calculates the speed of a certain pokemon
+        #apply speed boosts
+        boost = pokemon.boosts.get("spe", 0)
+        speed = pokemon.stats["spe"]
+        if boost > 0:
+            speed *= (2 + boost) / 2
+        elif boost < 0:
+            speed *= 2 / (2 - boost)
+        #apply paralysis
+        if "par" in pokemon.status:
+            speed /= 2
+        if pokemon.item == "choicescarf":
+            speed *= 1.5
+        #doesnt account for abilities, terrain, weather, etc
+        return speed
